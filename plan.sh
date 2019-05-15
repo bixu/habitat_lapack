c582c013d8af945b079efb41c8f80802738e6fe6
Blake Irvin <blakeirvin@me.com>
2019-05-15 12:34:30 +0200

    update to minimally-working Plan

--- plan.sh
+++ plan.sh
@@ -1,90 +1,60 @@
 pkg_origin=bixu
 pkg_name=lapack
 pkg_version="3.8.0"
-pkg_maintainer="Jeff Moody <fifthecho@gmail.com>, Blake Irvin <blakeirvin@me.com>"
-pkg_license=("http://www.netlib.org/lapack/LICENSE.txt")
-pkg_source="http://www.netlib.org/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz"
+pkg_license=("Lapack")
+pkg_source="https://www.netlib.org/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz"
 pkg_shasum="deb22cc4a6120bff72621155a9917f485f96ef8319ac074a7afbc68aab88bcf6"
 pkg_lib_dirs=("lib64")
-pkg_pconfig_dirs=("lib64/pkgconfig")
 pkg_description="Linear Algebra PACKage"
 pkg_upstream_url="http://www.netlib.org/lapack/"
-
-pkg_build_deps=(
-  "core/cmake"
-  "core/gcc"
-  "core/make"
-  "core/pkg-config"
-  "core/python2"
-)
+pkg_lib_dirs=("lib64")
+pkg_include_dirs=("include")
+pkg_pconfig_dirs=("lib64/pkgconfig")
 pkg_deps=(
-  "core/gcc-libs"
+  "core/doxygen"
+  "core/gcc"
+  "core/cmake"
+  "core/make"
+  "core/python"
   "core/glibc"
-  "core/zlib"
 )
 
-do_setup_environment(){
-  push_buildtime_env LIBRARY_PATH "$(pkg_path_for core/glibc)/lib"
-  push_buildtime_env LIBRARY_PATH "$(pkg_path_for core/gcc-libs)/lib"
+do_setup_environment() {
+  set_buildtime_env LIBRARY_PATH "$(hab pkg path core/glibc)/lib"
   return $?
 }
 
 do_prepare() {
-  push_buildtime_env FC "gfortran"
-  do_default_prepare
+  install -d build
   return $?
 }
 
 do_build() {
-  if [ ! -d "build" ]
-  then
-    mkdir -p "build"
-  fi
-  pushd "build" || exit 1
-    cmake \
-        -D BUILD_DEPRECATED="ON" \
-        -D BUILD_SHARED_LIBS="ON" \
-        -D BUILD_TESTING="OFF" \
-        -D CBLAS="ON" \
-        -D CMAKE_BUILD_TYPE="Release" \
-        -D CMAKE_Fortran_COMPILER="gfortran" \
-        -D CMAKE_INSTALL_PREFIX="$pkg_prefix" \
-        -D CMAKE_SKIP_RPATH="ON" \
-        -D LAPACKE_WITH_TMG="ON" \
-      ..
+  cd build
+    cmake /hab/cache/src/$pkg_dirname -DCMAKE_BUILD_TYPE=Release -DCMAKE_SKIP_RPATH=ON -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=/ -DCMAKE_Fortran_COMPILER=gfortran -DLAPACKE_WITH_TMG=ON -DCBLAS=ON -DBUILD_DEPRECATED=ON
     make -j"$(nproc)"
-  popd || exit 1
-
-  pushd LAPACKE || exit 1
-  if [ ! -d "build" ]
-  then
-    mkdir -p "build"
-  fi
-    pushd "build" || exit 1
-      cmake \
-        -D BUILD_DEPRECATED="ON" \
-        -D BUILD_SHARED_LIBS="ON" \
-        -D BUILD_TESTING="OFF" \
-        -D CBLAS="ON" \
-        -D CMAKE_BUILD_TYPE="Release" \
-        -D CMAKE_Fortran_COMPILER="gfortran" \
-        -D CMAKE_INSTALL_PREFIX="$pkg_prefix" \
-        -D CMAKE_SKIP_RPATH="ON" \
-        -D LAPACKE_WITH_TMG="ON" \
-        ..
-      make -j"$(nproc)"
-    popd || exit 1
-  popd || exit 1
+  cd ..
   return $?
 }
 
 do_install() {
-  pushd "$HAB_CACHE_SRC_PATH/${pkg_name}-${pkg_version}/build" || exit 1
-    make install
-  popd || exit 1
+  cd build
+    make DESTDIR="$pkg_prefix" install
+  
+  cd BLAS
+    make DESTDIR="$pkg_prefix" install
+  cd ..
 
-  pushd "$HAB_CACHE_SRC_PATH/${pkg_name}-${pkg_version}/LAPACKE/build" || exit 1
-    make install
-  popd || exit 1
+  cd CBLAS
+    make DESTDIR="$pkg_prefix" install
+  cd ..
+
+  cd LAPACKE
+    make DESTDIR="$pkg_prefix" install
+  cd ..
+
+  mv "$pkg_prefix/usr/include" "$pkg_prefix/"
+  mv "$pkg_prefix/usr/lib64" "$pkg_prefix/"
+  rm -rf "$pkg_prefix/usr"
   return $?
 }
